f1911b2f09b0188722c130e24e4bda0be6e4974b
Kevin Dwyer <kevin@pheared.net>
2016-03-16 20:59:32 -0400

    Bump to 1.1 for the new changes.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'timehash',
   packages = ['timehash'], # this must be the same as the name above
-  version = '1.0',
+  version = '1.1',
   description = 'Module to encode and decode timestamps to/from TimeHashes',
   author = 'Abe Usher',
   author_email = 'abe.usher@gmail.com',

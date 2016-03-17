9864e904296d2038214a7fbf77f3bf6d6588f8a2
Kevin Dwyer <kevin@pheared.net>
2016-03-16 21:01:08 -0400

    Fix link

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Abe Usher',
   author_email = 'abe.usher@gmail.com',
   url = 'https://github.com/abeusher/timehash',
-  download_url = 'https://github.com/abeusher/timehash/tarball/1.0',
+  download_url = 'https://github.com/abeusher/timehash/tarball/1.1',
   keywords = ['timehash', 'datetime', 'time'],
   classifiers = [],
 )
